ec3c3177288307c7e4f7ce0dd900ea9bad027bb7
Elisabeth Virak <elisabethvirak@gmail.com>
2020-02-23 12:35:09 -0500

    Got it working as an integer

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,10 +6,9 @@ import os
 profit_loss = 0
 months = 0 
 monthly_profit_list = []
-month_ref = []
+month_max_profit = ["", 0]
+month_max_loss = ["", 0]
 avg_profloss = 0
-max_profit = 0
-max_loss = 0
 
 # create path to find csv file
 budget_csv = os.path.join("budget_data.csv")
@@ -22,47 +21,46 @@ with open(budget_csv, 'r', newline="") as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
 
     # grab header file
-    csv_header = next(csvfile)
+    csv_header = next(csv_reader)
     print(f"Header:{csv_header}")
 
     # define values to compare to
-    month = next(csvfile)
-    prev_net = float(month[1])
-
+    stats = next(csv_reader)
+    prev_net = int(stats[1])
+    
     # iterate through the rows
     for row in csv_reader:
-        
+
+        prev_change = int(prev_net)
+
         # find total profit_loss
         profit_loss = profit_loss + int(row[1])
 
         # add up months
         months = months + 1
 
-        prev_net = int(row)
         # set initial values to calculate monthly change
-        monthly_change = int(row[1]) - prev_net
+        monthly_change = int(row[1]) - prev_change
         # record monthly change to list
         monthly_profit_list.append('monthly_change')
-        # keep track of source of monthly change
-        month_ref.append(row[0])
         
         # find greatest increase in profits
-        if monthly_change > max_profit:
+        if (monthly_change > month_max_profit[1]):
             
             # store max profit value
-            max_profit = monthly_change
+            month_max_profit[1] = monthly_change
 
             # store month of max value
-            month_max_profit = str(row[0])
+            month_max_profit[0] = str(row[0])
         
         # find greatest decrease in profits
-        if monthly_change < max_loss:
+        if (monthly_change < month_max_loss[1]):
             
             # store max loss value
-            max_loss = monthly_change
+            month_max_loss[1] = monthly_change
 
             # store month of max loss
-            month_max_loss = str(row[0])
+            month_max_loss[0] = str(row[0])
     
 
     # format final table
@@ -71,25 +69,5 @@ with open(budget_csv, 'r', newline="") as csvfile:
     print("Total Months: " + str(months))
     print("Total: $" + str(profit_loss))
     print("Average Change: $" + str(avg_profloss))
-    print("Greatest Increase in Profits: " + month_max_profit + " ($" + str(max_profit) + ")")
-    print("Greatest Decrease in Profits: " + month_max_loss + " ($" + str(max_loss) + ")")
-
-
-'''    # open new file to write data to
-    with open(output, 'w', newline="") as datafile:
-        csv_writer= csv.writer(datafile, delimiter=",")
-
-        # write old csv file to new csv file
-        for row in csv_reader:
-            csv_writer.writerow(row)
-
-            # append row for changes
-            csv_writer.append(row)
-    # open csvfile to write to
-    with open(output, "w", newline="") as datafile:
-        writer = csv.writer(output, delimiter=",")
-        new_headers = csv_headers
-        print(f"Header:{new_headers}")
-
-
-'''
+    print("Greatest Increase in Profits: " + month_max_profit[0] + " ($" + str(month_max_profit[1]) + ")")
+    print("Greatest Decrease in Profits: " + month_max_loss[0] + " ($" + str(month_max_loss[1]) + ")")
