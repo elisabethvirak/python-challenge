477b264e47392a1b211e425cc868c28ee48a2bd7
Elisabeth Virak <elisabethvirak@gmail.com>
2020-02-23 13:59:46 -0500

    calculated and formatted kind of a little bit mostly maybe

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -10,6 +10,8 @@ correy_votes = 0
 khan_votes = 0
 li_votes = 0
 otooley_votes = 0
+winner = ""
+winning_votes = 0
 
 # create path to find csv file
 poll_csv = os.path.join("election_data.csv")
@@ -70,6 +72,20 @@ print("-------------------------")
 print("Total Votes: " + str(total_votes))
 print("-------------------------")
 
+# define winner dictionary for percentage and votes
+# iterate through candidates in results dictionary
+for candidate in results:
+    # grab candidate vote counts
+    vote = results.get(candidate)
+    print(f" {candidate}: {vote[0]:.3f}% {vote[1]:,}")
+    # compare candidate votes to winning votes
+    if (vote[1] > winning_votes):
+        winner = candidate
+        winning_votes = vote[1]
+# print(winner)
+
+print(f"Election Winner is: {winner} {winning_votes:,}")
+'''
 winner = [0,0]
 second = [0,0]
 third = [0,0]
@@ -78,7 +94,7 @@ fourth = [0,0]
 # sort candidates using dictionary
 if results[correy][1] > results
 
-''' Sort candidates based on total votes
+Sort candidates based on total votes
 if correy_perc > khan_perc:
     correy_perc = winner
     khan_perc = second
