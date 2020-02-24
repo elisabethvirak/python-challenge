fcc5ed567eb4368f1d92d8e1a306d2b2279a9d11
Elisabeth Virak <elisabethvirak@gmail.com>
2020-02-24 18:20:09 -0500

    Fixed printing to text file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -11,10 +11,14 @@ khan_votes = 0
 li_votes = 0
 otooley_votes = 0
 winner = ""
+winning_perc = 0
 winning_votes = 0
 
 # create path to find csv file
 poll_csv = os.path.join("election_data.csv")
+
+# create path to export text file
+output = os.path.join("Election_Results.txt")
 
 # open csv file using csv reader
 with open(poll_csv, 'r', newline="") as csvfile:
@@ -59,18 +63,13 @@ li_perc = (li_votes/total_votes)*100
 otooley_perc = (otooley_votes/total_votes)*100
 
 # vote results store in dictionary
-results = {
-    "Correy":[correy_perc, correy_votes],
-"Khan":[khan_perc,khan_votes],
-"Li":[li_perc,li_votes],
-"O'Tooley":[otooley_perc,otooley_votes]}
-print(results)
+results = {"Correy":[correy_perc, correy_votes],"Khan":[khan_perc,khan_votes],"Li":[li_perc,li_votes],"O'Tooley":[otooley_perc,otooley_votes]}
 
-# create election results
+# print election results
 print("Election Results")
-print("-------------------------")
+print("--------------------------------------")
 print("Total Votes: " + str(total_votes))
-print("-------------------------")
+print("--------------------------------------")
 
 # define winner dictionary for percentage and votes
 # iterate through candidates in results dictionary
@@ -81,48 +80,20 @@ for candidate in results:
     # compare candidate votes to winning votes
     if (vote[1] > winning_votes):
         winner = candidate
+        winning_perc = vote[0]
         winning_votes = vote[1]
 # print(winner)
+print("--------------------------------------")
+print(f"Election Winner is: {winner} with {winning_perc:.3f}% of the votes")
+print("--------------------------------------")
 
-print(f"Election Winner is: {winner} {winning_votes:,}")
-'''
-winner = [0,0]
-second = [0,0]
-third = [0,0]
-fourth = [0,0]
-
-# sort candidates using dictionary
-if results[correy][1] > results
-
-Sort candidates based on total votes
-if correy_perc > khan_perc:
-    correy_perc = winner
-    khan_perc = second
-else:
-    khan_perc = winner
-    correy_perc = second
-if li_perc > winner:
-    second = third
-    winner = second
-    li_perc = winner
-elif li_perc < second:
-    li_perc = third
-else:
-    second = third
-    li_perc = second
-if otooley_perc > winner:
-    third = fourth
-    second = third
-    winner = second
-elif otooley_perc < winner and otooley_perc > second:
-    third = fourth
-    second = third
-    otooley_perc = second
-elif otooley_perc < second and > third:
-    third = fourth
-    otooley_perc = third
-else:
-    otooley_perc = fourth
-
-print("First: " + str(winner) + "")
-'''
+with open(output, "w") as txt_file:
+    txt_file.write(f"Election Results\n"
+        f"--------------------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"--------------------------------------\n")
+    for candidate in results:
+        txt_file.write(f" {candidate}: {vote[0]:.3f}% {vote[1]:,}\n")
+    txt_file.write(f"--------------------------------------\n"
+        f"Election Winner is: {winner} with {winning_perc:.3f}% of the votes\n"
+        f"--------------------------------------\n")
